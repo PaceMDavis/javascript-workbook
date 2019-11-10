c60e891f0eb30a7e728310d258227d50afab50e0
Pace Davis <pacemdavis@gmail.com>
2019-11-10 11:56:40 -0600

    rewriting functions

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,75 +19,88 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece(startStack, endStack) {
+// array.splice(index, howmany, item1, ....., itemX)
+// Create a function that moves the last element in the array to a new array
+  function movePiece(startStack, endStack) {
   // Your code here
-  const move = stacks[startStack].pop();
-  stacks[endStack].push(move);
+  // Use splice to take the element at the last position in the array
+  // Use concat to add the element from startStack to a new Array
+  // Pass the new array into endStack
+  const removeStackElement = stacks[startStack].splice(-1, 1);
+  const newStack = stacks[endStack].concat(removeStackElement);
+  stacks[endStack] = newStack;
+  
+  //This was my original solution. Pop and push seem to be the most efficient solution
+  //Use pop to remove the element from the array, and push to add it to the new array
+  // const move = stacks[startStack].pop();
+  // stacks[endStack].push(move);
 
 }
 
 function isLegal(startStack, endStack) {
   // Your code here
-const startStackLength = stacks[startStack].length;
-const endStackLength = stacks[endStack].length;
+  // Create a variable to hold the length of the startStack and endStack
+  const startStackLength = stacks[startStack].length;
+  // const endStackLength = stacks[endStack].length;
+  // create two variables to hold the last element in the array using slice. 
+  const startStackLastElement = stacks[startStack].slice(-1)[0];
+  const endStackLastElement = stacks[endStack].slice(-1)[0];
+  // Create if/else statement that determines if a move is legal by looking at the startStack length, if 
+  // startStack and endStack are the same length and if the last element in the startStack array is greater
+  //than the last element in the endStack array. If any of these conditions are true return false and prevent
+  //the move from happening
+  if (
+    startStackLength === 0 ||
+    startStack === endStack ||
+    startStackLastElement > endStackLastElement
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+  // This was my first solution simply using length. I think it looks cleaner and is simpler to code
+  // But i wanted to practice using slice().
+  // if (
+  //   startStackLength === 0 ||
+  //   startStack === endStack ||
+  //   stacks[startStack][startStackLength - 1] > stacks[endStack][endStackLength - 1]
+  // ) { 
+  //   return false;
+  // } else {
+  //   return true;
+  // }
 
-if (
-  startStackLength === 0 ||
-  startStack === endStack ||
-  stacks[startStack][startStackLength - 1] > stacks[endStack][endStackLength - 1]
-) { 
-  return false;
-} else {
-  return true;
-}
-
-//   let stackReadA = stacks.a.slice(-1)[0];
-//   let stackReadB = stacks.b.slice(-1)[0];
-//   let stackReadC = stacks.c.slice(-1)[0];
-//   if (stackReadA || stackReadB || stackReadC < endStack){
-//     movePiece()
-//   } else {
-//     console.log("Invalid move! Try again!");
-//   }
-
-//   console.log(stacked);
-// }
 }
 function checkForWin() {
   // Your code here
+  //write a function that validates wins
+  // Create variable that stores the original length of stack a and uses it to determine if either b or c 
+  // reach that length. If so, return true and validate win.
   let winningStack = stacks.a.length;
   if(stacks.b.length === winningStack || stacks.c.length === winningStack) {
+  console.log("You're a winner!")
   return true;
   } return false;
 
 }
+// Create function that validates that you are accessing one of the stacks
 function isStack(startStack, endStack) {
   const validStacks = ['a', 'b', 'c'];
   if (validStacks.includes(startStack) && validStacks.includes(endStack)){
     return true;
-  }  
+  }  console.log("Please choose from one of the given stacks!");
     return false;
 }
-
+//Write a function to call in all other functions and determine what happens if you meet conditions.
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
-  //create nested for loops to loop over stack array and return values
-  // let startStack = stacks
-
+  // if/else statement that says if you are accessing a valid stack and the move is legal, allow the move and 
+  // check if the stacks qualify for a win. Otherwise, return an error message and do nothing.
   if (isLegal(startStack, endStack) && isStack(startStack, endStack)) {
     movePiece(startStack, endStack) && checkForWin(startStack, endStack);
   } else {
     console.log("Illegal move! Try again!");
   }
 } 
-  // let stackCheck = stacks.length;
-  // for (let i = 0; i < stackCheck; i++) {
-  //   let halfWay = stacks[i].length;
-  // for (let j = 0; j<halfWay; j++){
-  //   console.log(stacks[i][j])
-  // }
-  // }
-
 
 
 function getPrompt() {
@@ -110,6 +123,8 @@ if (typeof describe === 'function') {
       assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
       towersOfHanoi('a', 'c');
       assert.deepEqual(stacks, { a: [4, 3], b: [1], c: [2] });
+      towersOfHanoi('b', 'c');
+      assert.deepEqual(stacks, { a: [4, 3], b: [], c: [2, 1] });
     });
   });
 
@@ -129,6 +144,7 @@ if (typeof describe === 'function') {
         c: []
       };
       assert.equal(isLegal('a', 'c'), true);
+      assert.equal(isLegal('a', 'b'), true);
     });
   });
   describe('#checkForWin()', () => {
@@ -137,6 +153,17 @@ if (typeof describe === 'function') {
       assert.equal(checkForWin(), true);
       stacks = { a: [1], b: [4, 3, 2], c: [] };
       assert.equal(checkForWin(), false);
+    });
+  });
+  describe('#isStack()', () => {
+    it('should check that you have chosen a valid stack', () => {
+      stacks = {
+        a: [4, 3, 2, 1],
+        b: [],
+        c: []
+      };
+      assert.equal(isStack('a', 'b', 'c'), true);
+      assert.equal(isStack('d', 'horse'), false);
     });
   });
 
