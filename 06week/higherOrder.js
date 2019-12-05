f4c257e5af92a90896289707c16e0824aaf7a1f1
Pace Davis <pacemdavis@gmail.com>
2019-12-05 10:40:45 -0600

    finished

--- 06week/higherOrder.js
+++ 06week/higherOrder.js
@@ -5,19 +5,78 @@ const assert = require('assert');
 // function forEach(arr, callback) {
 //   // Your code here
 // }
-
-function map(arr, callback) {
+const arr = [1, 2, 3];
+//create a function to mimic map
+//arr represents array parameter
+//callback represents the function being passed in
+const map = (arr, callback) => {
+  //mapped is an empty array, which will be our altered array for the function
+  const mapped = [];
+  //for loop to iterate over the array
+  for (let i = 0; i< arr.length; i++) {
+    //push new mapped items to mapped array using callback function
+    mapped.push(callback(arr[i]))
+  }
+  //return new array
+  return mapped;
   // Your code here
 }
+// callback is the argument that will be passed in as the second parameter in function
+const myCallBack = (num) => num * num;
+console.log(map(arr, myCallBack));
+
 
 function filter(arr, callback) {
+  const filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    // if(arr[i] % 2 === 0) 
+    if(callback(arr[i]) === true) {
+    console.log(callback(arr[i]), 'hello');
+      filteredArr.push(arr[i]);
+    }
   // Your code here
-}
+} return filteredArr;
+ };
+const myFilter = num => {
+  if(num % 2 === 0) {
+   return true;
+  } 
+} 
+console.log(filter(arr, myFilter));
+
+// describe('#filter()', () => {
+//   it('should return an array of items that pass the predicate test', () => {
+//     const filtered = filter([1, 2, 3], (num) => {
+//       return num % 2 === 0;
+//     });
+//     assert.deepEqual(filtered, [2]);
+//   });
+// });
 
 const foodItems = [5.50, 7.65, 8.12, 23.12]
-function reduce(arr,callback) {
+
+const reduce = (arr, cb) => {
+  let currentValue = 0;
+  for(var i = 0; i < arr.length; i++) {
+    currentValue += arr[i];
+  } return currentValue;
   // Your code here
 }
+console.log(reduce(foodItems))
+
+// const reducer = (a, b) => a + b;
+
+// const myReduce = (list, initialValue, reducer) => {
+//   if(list.length === 0) {
+//     return initialValue;
+//   } else {
+//     const accumulator = list[0]
+//     const rest = list.slice(1);
+//     const updatedAccumulator = reducer(initialValue, accumulator)
+//     return myReduce(rest, updatedAccumulator, reducer)
+//   }
+// }
+// console.log(myReduce(foodItems, 0, reducer ))
 // function some(arr, callback) {
 //   // Your code here
 // }
