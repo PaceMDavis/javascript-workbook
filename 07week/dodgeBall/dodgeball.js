5de5972da537b46553c14ceb2cf3473083fa0079
Pace Davis <pacemdavis@gmail.com>
2019-12-13 13:03:59 -0600

    working

--- 07week/dodgeBall/dodgeball.js
+++ 07week/dodgeBall/dodgeball.js
@@ -57,71 +57,138 @@ const blueTeam = []
 const redTeam = []
 
 //Set up a class for player that includes canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience, and will need to extend when added to a team
-class player {
-  constructor(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
+class Player {
+  constructor(person, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
+    this.person = person;
     this.canThrowBall = canThrowBall;
     this.canDodgeBall = canDodgeBall;
     this.hasPaid = hasPaid;
     this.isHealthy = isHealthy;
     this.yearsExperience = yearsExperience;
     }
+assignPlayer(player) {
+  player.push(this)
+  this.player = player
 }
-
+}
+console.log(Player)
 // set up a class for blueTeammate that players can be added to
-class blueTeammate extends player {
-  constructor(color, mascot, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
-    super(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience);
-    this.color = color;
-    this.mascot = mascot;
+class BlueTeammate extends Player {
+  constructor(person, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
+    super(person, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience);
+    this.color = "Blue";
+    this.mascot = "Bluejay";
   }
 }
 //set up a class for redTeammate that players can be added to
-class redTeammate extends player {
-  constructor(color, mascot, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
-    super(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience);
-    this.color = color;
-    this.mascot = mascot;
+class RedTeammate extends Player {
+  constructor(person, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
+    super(person, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience);
+    this.color = "Red";
+    this.mascot = "Cardinal";
   }
 }
 // This function populates the first list of people when clicked
 const listPeopleChoices = () => {
+  //This grabs the ul people
   const listElement = document.getElementById('people')
+  listElement.innerHTML =''
+  //This function maps over the array of people with person as its parameter
   arrOfPeople.map(person => {
+    //This creates a new list element
     const li = document.createElement("li")
+    //This creates a new button
     const button = document.createElement("button")
+    //The test of the new button is Make Player
     button.innerHTML = "Make Player"
+    //This adds an event listener to the button and that is click, so when 
+    //The button is clicked it will call the makePlayer function
     button.addEventListener('click', function() {makePlayer(person.id)} )
+    //The new li that has been created is appended with the new button
     li.appendChild(button)
+    //It is then appended with the players name and skillset
     li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+    //The li is appended to the UL with the ID people from above
     listElement.append(li)
-    listOfPlayers.push(person)
+    // listOfPlayers.push(person)
+    
   }); console.log(listOfPlayers)
 }
 
 
 const makePlayer = (id) => {
   const dodgePlayers = document.getElementById('players')
-  arrOfPeople.map(person => {
-    playerMove = document.getElementById(`${id}`)
+  let index = arrOfPeople.findIndex(people => {
+    return people.id === id
+  })
+  let player = new Player(arrOfPeople[index], true, true, true, true, 4)
+  listOfPlayers.push(player)
+  arrOfPeople.splice(index, 1)
+  displayPlayer()
+  listPeopleChoices();
+  console.log(`li ${id} was clicked!`)
+}
+const displayPlayer= () => {
+  const dodgePlayers = document.getElementById('players')
+  dodgePlayers.innerHTML=''
+  listOfPlayers.map(person => {
+    console.log('MAP', person)
+    playerMove = document.getElementById(`${person.person.id}`)
     const li = document.createElement("li")
     const redButton = document.createElement("button")
     const blueButton = document.createElement("button")
     redButton.innerHTML = "Red Team"
     blueButton.innerHTML = "Blue Team"
-    redButton.addEventListener('click', function() {redTeamAssign(person.id)})
-    blueButton.addEventListener('click', function() {blueTeamAssign(person.id)})
+    redButton.addEventListener('click', function() {redTeamAssign(person.person.id)})
+    blueButton.addEventListener('click', function() {blueTeamAssign(person.person.id)})
     li.appendChild(redButton)
     li.appendChild(blueButton)
-    li.appendChild(document.createTextNode(`${person.name} - ${person.skillSet}`))
+    li.appendChild(document.createTextNode(`${person.person.name} - ${person.person.skillSet}`))
     dodgePlayers.append(li)
   })
-  console.log(`li ${id} was clicked!`)
 }
-
+const displayRed = () => {
+  const redPlayers = document.getElementById('red')
+  redPlayers.innerHTML=''
+  redTeam.map(person => {
+    // playerMove = document.getElementById(`${id}`)
+    const li = document.createElement("li")
+    li.appendChild(document.createTextNode(`${person.person.name} - ${person.person.skillSet}`))
+    redPlayers.append(li)
+  })
+}
 const redTeamAssign = (id) => {
-  console.log(`li ${id} was clicked red`)
+  const redPlayers = document.getElementById('red')
+  let index =listOfPlayers.findIndex(person => {
+    return person.id= id
+  })
+  let player = new RedTeammate(listOfPlayers[index], true, true, true, true, 4)
+  redTeam.push(player.person)
+  listOfPlayers.splice(index,1)
+  displayRed()
+  displayPlayer()
+  console.log(redTeam)
 }
-
+const displayBlue = () => {
+  const bluePlayers = document.getElementById('blue')
+  bluePlayers.innerHTML=''
+  blueTeam.map(person => {
+    console.log(person, "I'm here")
+    const li = document.createElement("li")
+    li.appendChild(document.createTextNode(`${person.person.name} - ${person.person.skillSet}`))
+    bluePlayers.append(li)
+  })  
+}
+// const displayBlue
 const blueTeamAssign = (id) => {
-  console.log(`li ${id} was clicked blue`)
+  const bluePlayers = document.getElementById('blue')
+  let index =listOfPlayers.findIndex(person => {
+    return person.id= id
+  })
+  let player = new BlueTeammate(listOfPlayers[index], true, true, true, true, 4)
+  blueTeam.push(player.person)
+  listOfPlayers.splice(index,1)
+  displayBlue()
+  displayPlayer()
+  console.log(blueTeam)
 }
